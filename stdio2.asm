tubesnob-apple/orca

commit fc82a34c3ebd361cec7a152296805181a38c16b3

Support L size modifier for long double conversions in scanf.
This fixes #1.

stdio2.asm
@@ -364,6 +364,11 @@ aexp     ds    2
          using ~scanfCommon
          using ~printfCommon
 arg      equ   11                       argument
+
+FX2X_op  equ   $0010                    SANE opwords for conversions
+FX2D_op  equ   $0110
+FX2S_op  equ   $0210
+
 ;
 ;  Read the ASCII version of the number
 ;
@@ -454,10 +459,14 @@ dg2      ph4   #0                       convert to an extended number
          ph4   arg
          dec   ~size
          beq   lb11
-         fx2s
+         bmi   lb11a
+         ph2   #FX2X_op
          bra   lb12
-lb11     fx2d
-lb12     lda   ~suppress                quit if output is suppressed
+lb11     ph2   #FX2D_op
+         bra   lb12
+lb11a    ph2   #FX2S_op
+lb12     _SANEFP816
+         lda   ~suppress                quit if output is suppressed
          bne   lb13
          ldy   #2                       remove the parameter from the stack
          jsr   ~RemoveWord

stdio2.macros
@@ -204,18 +204,6 @@
  LDX #$090A
  JSL $E10000
  MEND
- MACRO
-&LAB FX2S
-&LAB PEA $0210
- LDX #$090A
- JSL $E10000
- MEND
- MACRO
-&LAB FX2D
-&LAB PEA $0110
- LDX #$090A
- JSL $E10000
- MEND
  macro
 &l add2 &n1,&n2,&n3
  aif c:&n3,.a
@@ -301,3 +289,8 @@
 .b
 &l sta &op
  mend
+ MACRO
+&lab _SANEFP816
+&lab ldx #$090A
+ jsl $E10000
+ MEND

commit 567ae146c4543c05ad829395f51e51fb567b2545

scanf: Fix handling of non-matching inputs to floating-point conversions.
This was broken in commit fc82a34c3eb.

stdio2.asm
@@ -480,12 +480,12 @@ dg2      ph4   #0                       convert to an extended number
          beq   lb11
          bmi   lb11a
          ph2   #FX2X_op
-         bra   lb12
+         bra   lb11b
 lb11     ph2   #FX2D_op
-         bra   lb12
+         bra   lb11b
 lb11a    ph2   #FX2S_op
-lb12     _SANEFP816
-         lda   ~suppress                quit if output is suppressed
+lb11b    _SANEFP816
+lb12     lda   ~suppress                quit if output is suppressed
          bne   lb13
          ldy   #2                       remove the parameter from the stack
          jsr   ~RemoveWord